c03f2c09ae6fc80670ddf8b75e3e8f090c19105b
YashKharalkar <yashkharalkar02@gmail.com>
2026-07-03 11:11:44 +0530

    Code change-2 changed cors in backend

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(helmet({
 
 // CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://school-system-udzq.vercel.app'],
   credentials: true
 }));
 
